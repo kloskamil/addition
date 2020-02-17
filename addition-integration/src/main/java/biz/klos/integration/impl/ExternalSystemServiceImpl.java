c21b1181beb6988dbb088ce497db32430ac292d7
Kamil Kłos <kamil.klos@entiro.pl>
2020-02-17 19:59:26 +0100

    Replace external system url to properties

--- addition-integration/src/main/java/biz/klos/integration/impl/ExternalSystemServiceImpl.java
+++ addition-integration/src/main/java/biz/klos/integration/impl/ExternalSystemServiceImpl.java
@@ -1,13 +1,19 @@
 package biz.klos.integration.impl;
 
 import biz.klos.integration.ExternalSystemService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
 @Service
+@PropertySource("classpath:application.properties")
 public class ExternalSystemServiceImpl implements ExternalSystemService {
+
+    @Value("${external.system.url}")
+    private String url;
 
     @Override
     public BigDecimal getExternalNumber() {
@@ -15,20 +21,13 @@ public class ExternalSystemServiceImpl implements ExternalSystemService {
     }
 
     private String applyNewLineFilter(String string) {
-        string = string.substring(0, string.length() - 1);
-        return string;
+        return string.substring(0, string.length() - 1);
     }
 
     private BigDecimal getNumberFromApi() {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://www.random.org/integers/?num=1&min=1&max=999&col=1&base=10&format=plain&rnd=new";
         String value = restTemplate.getForObject(url, String.class);
         value = applyNewLineFilter(value);
-        BigDecimal numberFromApi = null;
-        if (value != null) {
-            numberFromApi = new BigDecimal(value);
-        }
-        return numberFromApi;
+        return new BigDecimal(value);
     }
-
 }
